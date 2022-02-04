f9e53d7c301c9809bc50186528bf443d16a59c42
Ignacio Navarro Blázquez <inavarroblazquez@gmail.com>
2022-02-04 11:53:49 +0100

    Added depencencies. Transitions. Liquid Swipe.

---
+++ app/src/main/java/com/example/zest/IntroActivity.java
@@ -0,0 +1,79 @@
+package com.example.zest;
+
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class IntroActivity extends AppCompatActivity {
+
+    ImageView logo, splashimg;
+    LottieAnimationView lottieAnimationView;
+
+    private static final int NUM_PAGES = 3;
+    private ViewPager viewPager;
+    private ScreenSlidePagerAdapter pagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        //hooks
+        logo = findViewById(R.id.logo);
+        splashimg = findViewById(R.id.background);
+        lottieAnimationView = findViewById(R.id.lottie);
+
+        viewPager = findViewById(R.id.pager);
+        pagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+
+        splashimg.animate().translationY(-5000).setDuration(1500).setStartDelay(1000);
+        logo.animate().translationY(-700).setDuration(1000).setStartDelay(1500);
+        lottieAnimationView.animate().translationY(5000).setDuration(1000).setStartDelay(1500);
+
+
+    }
+
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter{
+
+
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    OnBoardingFragment1 tab1 = new OnBoardingFragment1();
+                    return tab1;
+
+                case 1:
+                    OnBoardingFragment2 tab2 = new OnBoardingFragment2();
+                    return tab2;
+
+                case 2:
+                    OnBoardingFragment3 tab3 = new OnBoardingFragment3();
+                    return tab3;
+
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+    }
+
+}
